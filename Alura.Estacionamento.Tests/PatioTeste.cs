47d6b2136dd2b6b255be92ffebe27264ce0cc6e4
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2024-01-03 05:20:58 -0300

    adicionado theory nos testes

--- Alura.Estacionamento.Tests/PatioTeste.cs
+++ Alura.Estacionamento.Tests/PatioTeste.cs
@@ -6,20 +6,41 @@ namespace Alura.Estacionamento.Tests
 {
     public class PatioTeste
     {
-        [Fact]
-        public void Valida_Faturamento()
+        [Theory]
+        [InlineData("André Silva", TipoVeiculo.Automovel, "Verde", "Fusca", "qwe-7894")]
+        [InlineData("Renata Barros", TipoVeiculo.Automovel, "Vermelho", "Fiesta", "jki-2165")]
+        public void Valida_Faturamento_Com_Automovel(string proprietario, TipoVeiculo tipo, string cor, string modelo, string placa)
         {
             // Arrange
             Patio estacionamento = new Patio();
-            Veiculo veiculo = new Veiculo { Proprietario = "André Silva", Tipo = TipoVeiculo.Automovel, Cor = "Verde", Modelo = "Fusca", Placa = "qwe-7894" };           
+            Veiculo veiculo = new Veiculo { Proprietario = proprietario, Tipo = tipo, Cor = cor, Modelo = modelo, Placa = placa };
             estacionamento.RegistrarEntradaVeiculo(veiculo);
-            estacionamento.RegistrarSaidaVeiculo("qwe-7894");
+            estacionamento.RegistrarSaidaVeiculo(placa);
 
             // Act
             double faturamento = estacionamento.TotalFaturado();
 
             // Assert
             Assert.Equal(2, faturamento);
+        }
+
+        [Theory]
+        [InlineData("Primila Lima", TipoVeiculo.Motocicleta, "Rosa", "Honda", "vbn-5951")]
+        [InlineData("Ingriddy Santos", TipoVeiculo.Motocicleta, "Vermelho", "Yamaha", "hgf-3695")]
+        [InlineData("Kimberly Stefani", TipoVeiculo.Motocicleta, "Amarelo", "Yamaha", "cxz-1425")]
+        public void Valida_Faturamento_Com_Motocicleta(string proprietario, TipoVeiculo tipo, string cor, string modelo, string placa)
+        {
+            // Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo { Proprietario = proprietario, Tipo = tipo, Cor = cor, Modelo = modelo, Placa = placa };
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(placa);
+
+            // Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            // Assert
+            Assert.Equal(1, faturamento);
         }
     }
 }
